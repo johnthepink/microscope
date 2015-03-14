ea7731802695a9903c1e4479dd858d7f567e251d
John Pinkerton <jpinkerton88@gmail.com>
2015-03-14 10:24:38 -0400

    fix problems in bitly functionality

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -4,7 +4,7 @@ Bitly.shortenURL = function(url) {
   if(!Meteor.settings.bitly)
     throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
 
-  var shortenReponse = Meteor.http.get(
+  var shortenResponse = Meteor.http.get(
     "https://api-ssl.bitly.com/v3/shorten?",
     {
       timeout: 5000,
@@ -17,7 +17,7 @@ Bitly.shortenURL = function(url) {
   );
 
   if (shortenResponse.statusCode === 200) {
-    return shortenReponse.data.data.url;
+    return shortenResponse.data.data.url;
   } else {
     throw new Meteor.Error(500, "Bitly call failed with error: " + shortenResponse.status_txt);
   }
